1d352d682d8f7244b91c8e9001500650e9238395
jadDAOUK <jad190201.daouk@gmail.com>
2020-05-12 10:19:27 +0200

    confirmation

--- app/controllers/homepage.controller.php
+++ app/controllers/homepage.controller.php
@@ -9,14 +9,14 @@ use function Helpers\getRenderer;
 function getHomepageController(){
     if(isset($_SESSION["id"]) && isset($_SESSION["failed"]) && $_SESSION["failed"] == false){
         $twig = getRenderer();
-        echo $twig->render('homepage.html', ["Session" => $_SESSION["id"], "Group" => $_SESSION["group"]]);
+        echo $twig->render('homepage.html', ["Session" => $_SESSION["id"], "Group" => $_SESSION["group"], "Token" => $_SESSION["tokenactivated"]]);
     }else if(isset($_SESSION["failed"]) && ($_SESSION["failed"] == "mdp" || $_SESSION["failed"] == "user")){
         $twig = getRenderer();
-        echo $twig->render('homepage.html', ["Failed" => $_SESSION["failed"]]);
+        echo $twig->render('homepage.html', ["Failed" => $_SESSION["failed"], "Token" => $_SESSION["tokenactivated"]]);
         $_SESSION["failed"] = false;
     }else{
         $twig = getRenderer();
-        echo $twig->render('homepage.html');
+        echo $twig->render('homepage.html', ["Token" => $_SESSION["tokenactivated"]]);
     }
     
 }
--- app/models/inscription.model.php
+++ app/models/inscription.model.php
@@ -6,6 +6,7 @@ use Google_Service_Calendar_Calendar;
 use function Helpers\getDatabaseConnection;
 
 function setUser($Email, $Password, $Prenom, $Nom, $Adresse, $CodePostal, $Ville, $Codecat){
+    $token= kodex_random_string();
     $uniquemail = 1;
     $qcheckmail = getDatabaseConnection()->prepare("SELECT users.IDuser FROM users where users.email = :email");
     $qcheckmail->execute([
@@ -34,7 +35,7 @@ function setUser($Email, $Password, $Prenom, $Nom, $Adresse, $CodePostal, $Ville
             $rule->setRole("reader");
 
             $createdRule = $service->acl->insert($createdCalendar->getId(), $rule);
-            $qinscription = getDatabaseConnection()->prepare("INSERT INTO users(email,PassWord,Prenom,Nom,Adresse,CP,Ville,Codecat, CalendarId) VALUES(:email,:password,:prenom,:nom,:adresse,:cp,:ville,:codecateg,:calendar)");
+            $qinscription = getDatabaseConnection()->prepare("INSERT INTO users(email,PassWord,Prenom,Nom,Adresse,CP,Ville,Codecat, CalendarId, token) VALUES(:email,:password,:prenom,:nom,:adresse,:cp,:ville,:codecateg,:calendar,:token)");
             $qinscription->execute([
                 "email" => $Email,
                 "password" => $hashpassword,
@@ -44,9 +45,35 @@ function setUser($Email, $Password, $Prenom, $Nom, $Adresse, $CodePostal, $Ville
                 "cp" => $CodePostal,
                 "ville" => $Ville,
                 "codecateg" => $Codecat,
-                "calendar" => $createdCalendar->getId()
+                "calendar" => $createdCalendar->getId(),
+                "token" => $token
             ]);
             $qinscription->closeCursor();
+        $emailconfirm = new \SendGrid\Mail\Mail();
+        $emailconfirm->setFrom("contact@electrorepair.com", "Electro Repair");
+        $emailconfirm->setSubject("Electro Repair | Confirmation création votre compte");
+        $emailconfirm->addTo($Email, "Utilisateur");
+        $emailconfirm->addContent("text/html", " <br>
+        Bonjour <br>
+        Veuillez confirmé votre email en cliquant sur le lien ci-dessous: <br>
+        <a href='http://maritleawz.cluster028.hosting.ovh.net/?page=confirmation&token=".$token."'>Confirmé</a>");
+        $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+        try {
+            $response = $sendgrid->send($emailconfirm);
+            print $response->statusCode() . "\n";
+            print_r($response->headers());
+            print $response->body() . "\n";
+        } catch (Exception $e) {
+            echo 'Caught exception: '. $e->getMessage() ."\n";
+        }
     }
     echo (($uniquemail == 1) ? "le nouvel utilisateur a été créé" : "l'utilisateur existe déjà");
+}
+function kodex_random_string($length=30){
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $string = '';
+    for($i=0; $i<$length; $i++){
+        $string .= $chars[rand(0, strlen($chars)-1)];
+    }
+    return $string;
 }
